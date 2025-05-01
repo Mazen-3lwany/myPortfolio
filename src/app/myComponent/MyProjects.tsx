cd6acd2ac32ad1e7b2b946b66eb9021da32be881
Mazen-3lwany <116226312+Mazen-3lwany@users.noreply.github.com>
2025-05-01 15:39:30 +0300

    Update MyProjects.tsx

--- src/app/myComponent/MyProjects.tsx
+++ src/app/myComponent/MyProjects.tsx
@@ -1,4 +1,4 @@
-import React, { JSX } from 'react';
+
 import ProjectCard from './ProjectCard';
 import Prayer from '../assets/prayer.jpg';
 import ToDo from '../assets/ToDo.jpg';
@@ -8,14 +8,9 @@ import Social from '../assets/social.png';
 import { DiHtml5, DiCss3, DiBootstrap, DiJavascript, DiReact } from 'react-icons/di';
 import { SiTailwindcss } from 'react-icons/si';
 
-// Type for the project tech array, which will hold React icons
-type ProjectTech = JSX.Element[];
 
-interface MyProjectsProps {
-  // You can add more props if required here
-}
 
-const MyProjects: React.FC<MyProjectsProps> = () => {
+const MyProjects: React.FC = () => {
     return (
     <div id='Projects' className="mt-44 flex flex-col gap-2 relative ">
       {/* Header */}
